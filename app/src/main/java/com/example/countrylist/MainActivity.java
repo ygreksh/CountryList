7e9de3cd53265c86ff0f23a49d218fff819b1b4f
ygrek <ygreksh@gmail.com>
2023-10-20 12:20:02 +0300

    add empty fragments

--- app/src/main/java/com/example/countrylist/MainActivity.java
+++ app/src/main/java/com/example/countrylist/MainActivity.java
@@ -16,40 +16,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        countries.add(new Country("Name 1", "Capital 1", R.drawable.ar_flag));
-        countries.add(new Country("Name 2", "Capital 2", R.drawable.ar_flag));
-        countries.add(new Country("Name 3", "Capital 3", R.drawable.ar_flag));
-        countries.add(new Country("Name 4", "Capital 4", R.drawable.ar_flag));
-        countries.add(new Country("Name 5", "Capital 5", R.drawable.ar_flag));
-        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
-        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
-        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
-        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
-        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
-        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
-        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
-        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
-        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
-        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
-        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
-        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
-        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
-
-        RecyclerView recyclerView = findViewById(R.id.country_list);
-
-        CountryRecyclerViewAdapter countryListAdapter = new CountryRecyclerViewAdapter(this, countries);
-        recyclerView.setAdapter(countryListAdapter);
-
-        countryListAdapter.setOnCountryClickListener(new CountryRecyclerViewAdapter.OnCountryClickListener() {
-            @Override
-            public void onClick(int position, Country model) {
-                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
-                intent.putExtra("country_name", model.name);
-                intent.putExtra("country_capital", model.capital);
-                intent.putExtra("country_flag", model.flagResource);
-                startActivity(intent);
-            }
-        });
+//        countries.add(new Country("Name 1", "Capital 1", R.drawable.ar_flag));
+//        countries.add(new Country("Name 2", "Capital 2", R.drawable.ar_flag));
+//        countries.add(new Country("Name 3", "Capital 3", R.drawable.ar_flag));
+//        countries.add(new Country("Name 4", "Capital 4", R.drawable.ar_flag));
+//        countries.add(new Country("Name 5", "Capital 5", R.drawable.ar_flag));
+//        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
+//        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
+//        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
+//        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
+//        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
+//        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
+//        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
+//        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
+//        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
+//        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
+//        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
+//        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
+//        countries.add(new Country("Name 6", "Capital 6", R.drawable.ar_flag));
+//
+//        RecyclerView recyclerView = findViewById(R.id.country_list);
+//
+//        CountryRecyclerViewAdapter countryListAdapter = new CountryRecyclerViewAdapter(this, countries);
+//        recyclerView.setAdapter(countryListAdapter);
+//
+//        countryListAdapter.setOnCountryClickListener(new CountryRecyclerViewAdapter.OnCountryClickListener() {
+//            @Override
+//            public void onClick(int position, Country model) {
+//                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+//                intent.putExtra("country_name", model.name);
+//                intent.putExtra("country_capital", model.capital);
+//                intent.putExtra("country_flag", model.flagResource);
+//                startActivity(intent);
+//            }
+//        });
     }
 
 }
