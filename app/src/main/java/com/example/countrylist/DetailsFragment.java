f1544cbed83390da4ad4bdd643d248f08e6a19b5
ygrek <ygreksh@gmail.com>
2023-10-23 10:03:58 +0300

    +++ FragmentDetailsDataBinding

--- app/src/main/java/com/example/countrylist/DetailsFragment.java
+++ app/src/main/java/com/example/countrylist/DetailsFragment.java
@@ -2,6 +2,7 @@ package com.example.countrylist;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -12,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.countrylist.databinding.FragmentDetalsBinding;
 
 public class DetailsFragment extends Fragment {
     public static final String ARG_PARAM_NAME = "country_name";
@@ -40,7 +42,7 @@ public class DetailsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_detals, container, false);
+//        View view = inflater.inflate(R.layout.fragment_detals, container, false);
 
         country = new Country(
                 mParamName,
@@ -49,6 +51,10 @@ public class DetailsFragment extends Fragment {
         );
 
 
+
+        FragmentDetalsBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detals, container, false);
+        View view = binding.getRoot();
+        binding.setCountry(country);
 
         // Inflate the layout for this fragment
         return view;
